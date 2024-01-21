cc0ac6db7ed1844398ff6ece3d596d9b93b45dff
guoshijiang <guoshijiang2012@163.com>
2024-01-21 20:24:50 +0800

    debug optimistim

--- event/op_stack/common/types.go
+++ event/op_stack/common/types.go
@@ -7,17 +7,23 @@ import (
 )
 
 var (
-	LegacyStateCommitmentChain      = common.HexToAddress("0x2a3DD3EB832aF982ec71669E178424b10Dca2EDe")
-	L2OutputOracleProxy             = common.HexToAddress("0x2a3DD3EB832aF982ec71669E178424b10Dca2EDe")
-	OptimismPortalProxy             = common.HexToAddress("0x2a3DD3EB832aF982ec71669E178424b10Dca2EDe")
-	L1CrossDomainMessengerProxy     = common.HexToAddress("0x2a3DD3EB832aF982ec71669E178424b10Dca2EDe")
-	L1StandardBridgeProxy           = common.HexToAddress("0x2a3DD3EB832aF982ec71669E178424b10Dca2EDe")
-	L2ToL1MessagePasser             = common.HexToAddress("0x2a3DD3EB832aF982ec71669E178424b10Dca2EDe")
-	L2CrossDomainMessenger          = common.HexToAddress("0x2a3DD3EB832aF982ec71669E178424b10Dca2EDe")
-	L2StandardBridge                = common.HexToAddress("0x2a3DD3EB832aF982ec71669E178424b10Dca2EDe")
-	LegacyCanonicalTransactionChain = common.HexToAddress("0x2a3DD3EB832aF982ec71669E178424b10Dca2EDe")
-	ETHWithdrawEventSignature       = "0x2849b43074093a05396b6f2a937dee8565b15a48a7b3d4bffb732a5017380af5"
-	ERC20WithdrawEventSignature     = "0x7ff126db8024424bbfd9826e8ab82ff59136289ea440b04b39a0df1b03b9cabf"
+	L1BedrockStartHeight = big.NewInt(0)
+	L2BedrockStartHeight = big.NewInt(0)
+
+	LegacyStateCommitmentChain      = common.HexToAddress("0x0000000000000000000000000000000000000000")
+	LegacyCanonicalTransactionChain = common.HexToAddress("0x0000000000000000000000000000000000000000")
+
+	OptimismPortalProxy         = common.HexToAddress("0xbEb5Fc579115071764c7423A4f12eDde41f106Ed")
+	L1CrossDomainMessengerProxy = common.HexToAddress("0x25ace71c97B33Cc4729CF772ae268934F7ab5fA1")
+	L1StandardBridgeProxy       = common.HexToAddress("0x99C9fc46f92E8a1c0deC1b1747d010903E884bE1")
+	L2OutputOracleProxy         = common.HexToAddress("0xdfe97868233d1aa22e815a266982f2cf17685a27")
+
+	L2ToL1MessagePasser    = common.HexToAddress("0x4200000000000000000000000000000000000016")
+	L2CrossDomainMessenger = common.HexToAddress("0x4200000000000000000000000000000000000007")
+	L2StandardBridge       = common.HexToAddress("0x4200000000000000000000000000000000000010")
+
+	ETHWithdrawEventSignature   = "0x2849b43074093a05396b6f2a937dee8565b15a48a7b3d4bffb732a5017380af5"
+	ERC20WithdrawEventSignature = "0x7ff126db8024424bbfd9826e8ab82ff59136289ea440b04b39a0df1b03b9cabf"
 )
 
 type DepositTx struct {
