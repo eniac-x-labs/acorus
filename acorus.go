cac7f32779976a78e81161769d2a3dca38e8288a
liutian <liutianv1@gmail.com>
2024-04-06 21:23:48 +0800

    fix code

--- acorus.go
+++ acorus.go
@@ -116,6 +116,7 @@ func (as *Acorus) Start(ctx context.Context) error {
 				return fmt.Errorf("failed to start relayer: %w", err)
 			}
 		}
+
 	}
 	if err := as.relayerBridgeRelation.Start(); err != nil {
 		return fmt.Errorf("failed to start relayerBridgeRelation: %w", err)
